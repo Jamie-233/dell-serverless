c907336f3336a930e81ebd72bd744c177f0ce0ce
Jenkin <zhaojiancun@yunquna.com>
2022-03-15 21:01:54 +0800

    style: modifyed style

--- src/admin/index.jsx
+++ src/admin/index.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import 'antd/dist/antd.min.css';
 import HomeManagement from './container/HomeManagement';
+import 'antd/dist/antd.min.css';
 import 'normalize.css';
 import './style.scss';
 
